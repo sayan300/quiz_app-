08d3ab52724d506963ce9dbf89e37958a2f607ec
jsayan571-gif <jsayan571@gmail.com>
2026-09-21 13:13:02 +0530

    Replace bcrypt with bcryptjs for cross-platform compatibility

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const path = require('path');
 require('dotenv').config();
 const pool = require('./db');
